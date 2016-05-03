f6d7f7e0bed3975b87313d7edd7caff047df7096
SeanMcWilliams <mcwillis@bc.edu>
2016-05-03 14:33:15 -0400

    fixed sql

--- include/dboperationAddDeliveryLoc.php
+++ include/dboperationAddDeliveryLoc.php
@@ -16,7 +16,7 @@
 		$dbc = connect_to_db( "crawfocc" );
 		$name = $_POST["name"];
 		$address = $_POST["address"];
-
+/*
 		$query = "SELECT * FROM DELIVERY_LOCATIONS WHERE LocationName = '$name'";
 		$result = perform_query($dbc, $query);
 		$a = mysqli_fetch_array( $result, MYSQLI_ASSOC );
@@ -26,7 +26,7 @@
 			<a href="../pages/deliveryLocations.html">Return to add new location</a>
 			<?php
 			die("");
-		}
+		} */
 		$query2 = "INSERT INTO DELIVERY_LOCATIONS (LocationName, LocationAddress) VALUES ('$name', '$address')";
 		perform_query($dbc, $query2);
 		echo "<h2>Location added successfully!</h2>";
