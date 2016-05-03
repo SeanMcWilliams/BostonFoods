4cc49ffeb47f54a9fcdce06bdf700bf78249e8dc
crawfocc <crawfocc@bc.edu>
2016-05-03 18:37:15 -0400

    fixed delivery
    fixed select menu and therefore delete delivery site

--- include/dboperationDeleteDeliveryLoc.php
+++ include/dboperationDeleteDeliveryLoc.php
@@ -1,8 +1,11 @@
 <?php
 	include ("dbconn.php");
 	$dbc = connect_to_db( "crawfocc" );
-	$loc = $_POST["locations"];
-
-	$query = "DELETE * FROM DELIVERY_LOCATIONS WHERE LocationName = '$loc'";
+	$loc = "\"".$_POST["locations"]."\"";
+	//echo "<br>" . $loc . "<br>";
+	$query = "DELETE FROM `DELIVERY_LOCATIONS` WHERE `Location_Name` = $loc;";
 	perform_query($dbc, $query);
-	echo "<h2>Location successfully deleted!";
+	echo "<h2>Location successfully deleted!</h2>";
+	echo "<a href=\"../home.php\">return home!</a><br>";
+	echo "<a href=\"../pages/deliveryLocations.html\">return to Deliveries!</a>";
+?>
