bb0cfdaaccada72e1c5a22f7a420771922089a4b
thebenkeen <keeneb@bc.edu>
2016-05-04 17:59:53 -0400

    CSS and more

--- home.php
+++ home.php
@@ -15,42 +15,49 @@ if(isset($_COOKIE["isAdmin"])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" type="text/css" href="css/home.css">
 	<meta charset="utf-8" />
 	<title>Home Page</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body background= "back.jpg">
 
+<div class="title">
 <?php
 
 if($userName != null){
-	echo "<h1>Hello $userName !</h1>";
-} else {
-	echo "<h1>Hello New User!</h1>";
+	echo "Hello $userName!";
+} 
+else {
+	echo "Hello New User!";
 }
 
 ?>
-	
+</div>
 	<ul>
 		<?php
+		echo "<div class=\"options\">";
+		if($admin == 1){
+			echo "<li><a href=\"include/admin.php\">Admin Page!</a></li> 
+			 <li><a href=\"pages/deliveryLocations.html\">Delivery Locations</a></li> 
+			 <li><a href=\"pages/deliverySchedule.html\">Delivery Schedule</a></li> 
+			 <li><a href=\"pages/boxes.html\">Boxes</a></li>"; 
+		}
+
 		if($userName != null){
-			echo "<li><a href=\"pages/order.html\"></a></li>";
-			echo "<li><a href=\"pages/logOut.html\">Log Out!</a></li>";
+			echo "<li><a  href=\"pages/order.html\">Order!</a></li>";
+			echo "<li><a  href=\"pages/logOut.html\">Log Out!</a></li>";
 		} else {
-			echo "<li><a href=\"pages/logIn.html\">Log in!</a></li>";
+			echo "<li><a  href=\"pages/logIn.html\">Log in!</a></li>";
 		}
 
 		?>
 		<li><a href="pages/createaccount.html">Create An Account!</a></li>
-		<?php
-
-		if($admin == 1){
-			echo "<li><a href=\"include/admin.php\">Admin Page!</a></li>";
-			echo "<li><a href=\"pages/deliveryLocations.html\">Delivery Locations</a></li>";
-		}
-
-
-
-		?>
+		<li><a href="pages/aboutUs.html">About Us</a></li>
+		</div>
 	</ul>
 </body>
 </html>
