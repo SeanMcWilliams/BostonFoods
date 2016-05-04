de988b9aa497b983d601357fa14cda53ec687ee6
SeanMcWilliams <mcwillis@bc.edu>
2016-05-04 19:26:50 -0400

    fixed home.php merge

--- home.php
+++ home.php
@@ -36,7 +36,10 @@ else {
 }
 
 ?>
-<<<<<<< HEAD
+<h1>Welcome to Boston Foods</h1>
+<h2>Boston Foods is a non-profit organization with a simple purpose:</h2>
+<h2>To sell delicious, healthy, high-quality foods at affordable prices, particularly to folks living in areas with limited access to nutritious eating options.</h2>
+
 </div>
 	<ul>
 		<?php
@@ -51,25 +54,7 @@ else {
 		if($userName != null){
 			echo "<li><a  href=\"pages/order.html\">Order!</a></li>";
 			echo "<li><a  href=\"pages/logOut.html\">Log Out!</a></li>";
-=======
-<h1>Welcome to Boston Foods</h1>
-<h2>Boston Foods is a non-profit organization with a simple purpose:</h2>
-<h2>To sell delicious, healthy, high-quality foods at affordable prices, particularly to folks living in areas with limited access to nutritious eating options.</h2>
-	
-	<ul>
-		<?php
 
-		if($admin == 1){
-			echo "<li><a href=\"include/admin.php\">Admin Page!</a></li>"; 
-			echo "<li><a href=\"pages/deliveryLocations.html\">Delivery Locations</a></li>"; //put this in the admin pannel
-			echo "<li><a href=\"pages/deliverySchedule.html\">Delivery Schedule</a></li>"; //put this in the admin pannel
-			echo "<li><a href=\"pages/boxes.html\">Boxes</a></li>"; //put this in the admin pannel
-		}
-
-		if($userName != null){
-			echo "<li><a href=\"pages/order.html\">Order!</a></li>";
-			echo "<li><a href=\"pages/logOut.html\">Log Out!</a></li>";
->>>>>>> origin/master
 		} else {
 			echo "<li><a  href=\"pages/logIn.html\">Log in!</a></li>";
 		}
@@ -77,10 +62,6 @@ else {
 		?>
 		<li><a href="pages/createaccount.html">Create An Account!</a></li>
 		<li><a href="pages/aboutUs.html">About Us</a></li>
-<<<<<<< HEAD
-		</div>
-=======
->>>>>>> origin/master
 	</ul>
 </body>
 </html>
