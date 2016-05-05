3a43a73748c252e7addc19dc573332a97dd96401
SeanMcWilliams <mcwillis@bc.edu>
2016-05-05 12:24:19 -0400

    Delete Delivery

---
+++ include/deleteDelivery.php
@@ -0,0 +1,13 @@
+<?php
+	include ("dbconn.php");
+	$dbc = connect_to_db( "crawfocc" );
+	$name = $_GET["name"];
+	$date = $_GET["date"];
+	$time = $_GET["time"];
+
+	$query = "DELETE FROM `DELIVERY_SCHEDULE` WHERE `LocationName` = $name AND 'DateT' = $date AND 'TimeT' = $time";
+	perform_query($dbc, $query);
+	echo "<h2>Delivery successfully deleted!</h2>";
+	echo "<a href=\"../home.php\">return home!</a><br>";
+	echo "<a href=\"../pages/deliveryLocations.html\">return to Deliveries!</a>";
+?>
