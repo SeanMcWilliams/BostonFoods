6ca5f7be1244c373a5f45439aabf2b74aab56155
crawfocc <crawfocc@bc.edu>
2016-05-05 13:00:07 -0400

    delete order db connection

--- include/dboperationDeleteOrder.php
+++ include/dboperationDeleteOrder.php
@@ -2,4 +2,19 @@
 
 print_r($_POST)
 
+	include ("dbconn.php");
+	$dbc = connect_to_db( "crawfocc" );
+	$id = $_POST["id"];
+	$exploded = explode("|",$_POST["cOSelect"]);
+	$box = $exploded[0];
+	$location = $exploded[1];
+	$date = exploded[2];
+
+	$query = "DELETE TOP 1 FROM `CURRENT_ORDERS` WHERE `CustID` = $id AND `BoxType` = '$box' AND DeliveryLocation = '$location' AND `PickupDate` = '$date';";
+	perform_query($dbc, $query);
+	echo "<h2>Location successfully deleted!</h2>";
+	echo "<a href=\"../home.php\">return home!</a><br>";
+	echo "<a href=\"../pages/deliveryLocations.html\">return to Deliveries!</a>";
+
+
 ?>
