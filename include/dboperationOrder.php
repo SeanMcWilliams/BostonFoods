007b8b15b0fb2df644cb01babce34bf55d33f377
crawfocc <crawfocc@bc.edu>
2016-05-05 04:26:16 -0400

    edits

--- include/dboperationOrder.php
+++ include/dboperationOrder.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>Join Page</title>
+	<title>Order Complete!</title>
 </head>
 <body>
 
@@ -14,9 +14,14 @@
 	handleForm();
 	function handleForm(){
 		$dbc = connect_to_db( "crawfocc" );
-		$box = $_POST["box"];
+		$firstName = $_POST["firstName"];
+		$firstName = $_POST["lastName"];
+		$ID = $_POST["ID"];
+		$boxType = (($_POST["box"]).explode("|"))[0];
+		$price = (($_POST["box"]).explode("|"))[1];
 
-		/*$query = "SELECT * FROM DELIVERY_LOCATIONS WHERE Location_Name = '$name'";
+
+		$query = "SELECT * FROM DELIVERY_LOCATIONS WHERE Location_Name = '$name'";
 		$result = perform_query($dbc, $query);
 		$a = mysqli_fetch_array( $result, MYSQLI_ASSOC );
 		if (mysqli_num_rows( $result ) != 0){
@@ -24,10 +29,11 @@
 			?> <br>
 			<a href="../pages/deliveryLocations.html">Return to add new location</a>
 			<?php
-			die("");*/
+			die("");
 		} 
-		$query2 = "INSERT INTO CURRENT_ORDERS (Location_Name, Location_Address) VALUES ('$name', '$address')";
+		$query2 = "INSERT INTO DELIVERY_LOCATIONS (Location_Name, Location_Address) VALUES ('$name', '$address')";
 		perform_query($dbc, $query2);
 		echo "<h2>Location added successfully!</h2>";
 		echo "<a href=\"../pages/deliveryLocations.html\">go back</a>";
 	}
+	
