1d924e60d45876f9fa1559422fe0b0e75a2b52df
crawfocc <crawfocc@bc.edu>
2016-05-05 00:58:54 -0400

    db connection working

--- include/insertDelivery.php
+++ include/insertDelivery.php
@@ -3,15 +3,15 @@
 include("dbconn.php");
 $dbc = connect_to_db( "crawfocc" );
 $location = $_POST["name"];
-$date = $_POST["date"];
-$time = $_POST["time"];
+$date = "\"".$_POST["date"]."\"";
+$time = "\"".$_POST["time"].":00\"";
 
 $q = "SELECT Location_Address FROM DELIVERY_LOCATIONS WHERE Location_Name = '$location'";
 $addressI = (perform_query($dbc, $q));
 $address = "\"" . ($addressI->fetch_object()->Location_Address) . "\"";
 $locationQ = "\"" . $location . "\"";
-$query = "INSERT INTO Delivery_Schedule (Date, Time, LocationName, LocationAddress) VALUES ($date, $time, $locationQ, $address);";
-echo $query;
+$query = "INSERT INTO `DELIVERY_SCHEDULE` (DateT, TimeT, LocationName, LocationAddress) VALUES ($date, $time, $locationQ, $address);";
+//echo $query;
 $result = perform_query($dbc, $query);
 echo "Delivery added to schedule";
 echo "<a href=\"../pages/deliverySchedule.html\">return to the previous page</a>";
