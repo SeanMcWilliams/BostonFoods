c3a3722df0905e0c68048e083a4ecc17c5d7b59e
crawfocc <crawfocc@bc.edu>
2016-05-05 11:45:39 -0400

    edits

--- include/queryOrders.php
+++ include/queryOrders.php
@@ -3,6 +3,11 @@
 include ("dbconn.php");
 $dbc = connect_to_db( "crawfocc" );
 $query = "select * from CURRENT_ORDERS";
+if(isset($_GET["id"])){
+	$id = $_GET["id"];
+	$query.=" WHERE CustID = $id;";
+}
+
 $result = mysqli_query($dbc, $query) or
 			die( "bad query".mysqli_error( $dbc ) );
  $orders_data = array();
